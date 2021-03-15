9e65a259dfa6a1e5d149d61c0ad9602f451e5156
Joseph Abraham <ekurubal24@gmail.com>
2021-03-15 23:04:55 +0300

    finding triangle types

--- overview_1.js
+++ overview_1.js
@@ -1,13 +1,27 @@
 // girilen 3 sayının ortalamasını bulan uygulama
 
-let firstNumber = parseInt(prompt("You entered a number"));
-let secondNumber = parseInt(prompt("You entered a number"));
-let thirdNumber = parseInt(prompt("You entered a number"));
-let numberTotal = firstNumber + secondNumber + thirdNumber;
-let numberAverage = numberTotal / 3;
+// let firstNumber = parseInt(prompt("You entered a number"));
+// let secondNumber = parseInt(prompt("You entered a number"));
+// let thirdNumber = parseInt(prompt("You entered a number"));
+// let numberTotal = firstNumber + secondNumber + thirdNumber;
+// let numberAverage = numberTotal / 3;
 
 
-console.log(`First number ${firstNumber} second number ${secondNumber} third number ${thirdNumber} total ${numberTotal} number average ${numberAverage}`);
+// console.log(`First number ${firstNumber} second number ${secondNumber} third number ${thirdNumber} total ${numberTotal} number average ${numberAverage}`);
 
+// üçgen çeşidini bulan uygulama
 
+let firstLength = parseInt(prompt("Entered a length"));
+let secondLength = parseInt(prompt("Enter one more length"));
+let thirdLength = parseInt(prompt("Enter one more length"));
+
+console.log(`first Length ${firstLength} second Length ${secondLength} third length ${thirdLength}`);
+
+if ((firstLength == secondLength) && (firstLength == thirdLength)) {
+    console.log("This triangle equal is a triangle");
+} else if ((firstLength != secondLength) && (secondLength != thirdLength) && (firstLength != thirdLength)) {
+    console.log("This triangle kind of side is a triangle");
+} else {
+    console.log("This triangle isosceles is a triangle") 
+}
 
