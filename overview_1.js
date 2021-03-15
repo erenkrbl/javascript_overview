479eb7ed42240830843ced1811fb1643c1f77baa
Joseph Abraham <ekurubal24@gmail.com>
2021-03-15 22:14:44 +0300

    Added finding an average

---
+++ overview_1.js
@@ -0,0 +1,13 @@
+// girilen 3 sayının ortalamasını bulan uygulama
+
+let firstNumber = parseInt(prompt("You entered a number"));
+let secondNumber = parseInt(prompt("You entered a number"));
+let thirdNumber = parseInt(prompt("You entered a number"));
+let numberTotal = firstNumber + secondNumber + thirdNumber;
+let numberAverage = numberTotal / 3;
+
+
+console.log(`First number ${firstNumber} second number ${secondNumber} third number ${thirdNumber} total ${numberTotal} number average ${numberAverage}`);
+
+
+
