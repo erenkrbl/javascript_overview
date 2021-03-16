4a036c7ad67397363b4ba0e51110792e0fb46973
Joseph Abraham <ekurubal24@gmail.com>
2021-03-16 21:37:24 +0300

    Converting to a letter system with if

--- overview_3.js
+++ overview_3.js
@@ -26,20 +26,44 @@
 
 // second solution
 
-let xValue = parseInt(prompt("Enter a value"));
-let yValue = parseInt(prompt("Enter a value"));
+// let xValue = parseInt(prompt("Enter a value"));
+// let yValue = parseInt(prompt("Enter a value"));
 
-let result = 0;
+// let result = 0;
 
-if (xValue > 0 && yValue < 0) {
-    result = 4 * xValue + 2 * yValue + 4;8
-} else if (xValue > 0 && yValue == 0) {
-    result = 2 * xValue - yValue + 3;
-} else if (xValue < 0 && yValue > 0) {
-    result = 3 * xValue + 4 * yValue + 3;
+// if (xValue > 0 && yValue < 0) {
+//     result = 4 * xValue + 2 * yValue + 4;
+// } else if (xValue > 0 && yValue == 0) {
+//     result = 2 * xValue - yValue + 3;
+// } else if (xValue < 0 && yValue > 0) {
+//     result = 3 * xValue + 4 * yValue + 3;
+// }
+
+// console.log(`Enter x value : ${xValue} and y value :  ${yValue} result: ${result}`)
+
+// 100'lük sisstemde verilen notları harfli sistemde gösteren uygulama
+
+const lessonGrade = parseInt(prompt("Enter your lesson grade ", "100"));
+
+
+if (lessonGrade >= 90) {
+    console.log("Your lesson grade AA")
+} else if ( lessonGrade >= 80 && lessonGrade < 90) {
+    console.log("your lesson grade BA")
+} else if ( lessonGrade >= 70 && lessonGrade < 80) {
+    console.log("your lesson grade BB")
+} else if ( lessonGrade >= 60 && lessonGrade < 70) {
+    console.log("your lesson grade CB")
+} else if ( lessonGrade >= 50 && lessonGrade < 60) {
+    console.log("your lesson grade CC")
+} else if ( lessonGrade >= 40 && lessonGrade < 50) {
+    console.log("your lesson grade DC")
+} else if ( lessonGrade >= 30 && lessonGrade < 40) {
+    console.log("your lesson grade DD")
+} else if ( lessonGrade >= 20 && lessonGrade < 30) {
+    console.log("your lesson grade FD")
+} else if (lessonGrade < 20) {
+    console.log("your lesson grade FF")
 }
-
-console.log(`Enter x value : ${xValue} and y value :  ${yValue} result: ${result}`)
-
 
 
