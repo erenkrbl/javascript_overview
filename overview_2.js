2e867ec534bc76ba42641e1ca0a5c53710315942
Joseph Abraham <ekurubal24@gmail.com>
2021-03-16 16:11:55 +0300

    Added writing the name with the while

--- overview_2.js
+++ overview_2.js
@@ -6,3 +6,12 @@ for (i = 0; i < 5; i++) {
     console.log((i+1) + " Arthur");
 }
 
+//while
+
+let count = 0;
+
+while(count < 5) {
+    console.log((count + 1) + " Arthur While")
+    count++;
+}
+
