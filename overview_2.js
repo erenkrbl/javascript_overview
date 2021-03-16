dd77e9046e8d87d3cacba24cb32cf07305f4d76b
Joseph Abraham <ekurubal24@gmail.com>
2021-03-16 16:29:04 +0300

    Added addition

--- overview_2.js
+++ overview_2.js
@@ -1,25 +1,35 @@
-// Döngülerle adınızın 5 defa yazdırlıması 
+// // Döngülerle adınızın 5 defa yazdırlıması 
 
-//for
+// //for
 
-for (i = 0; i < 5; i++) {
-    console.log((i+1) + " Arthur");
+// for (i = 0; i < 5; i++) {
+//     console.log((i+1) + " Arthur");
+// }
+
+// //while
+
+// let count = 0;
+
+// while(count < 5) {
+//     console.log((count + 1) + " Arthur While")
+//     count++;
+// }
+
+// // do while
+
+// let count1 = 0;
+// do {
+//     console.log((count1 + 1) + " Arthur do while");
+//     count1++;
+// } while(count1 <5);
+
+// 1'den 100'e kadar olan sayıların toplamı
+let total = 0;
+for(i = 1; i < 100; i++) {
+    total += i;
+    //total = total + i;
 }
+console.log("1'den 100'e kadar olan sayıların toplammı : " + total)
 
-//while
 
-let count = 0;
-
-while(count < 5) {
-    console.log((count + 1) + " Arthur While")
-    count++;
-}
-
-// do while
-
-let count1 = 0;
-do {
-    console.log((count1 + 1) + " Arthur do while");
-    count1++;
-} while(count1 <5);
 
