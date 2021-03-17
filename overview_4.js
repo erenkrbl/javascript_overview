e9e8316e68c834c8b472adad5af86eb377cbf045
Joseph Abraham <ekurubal24@gmail.com>
2021-03-17 17:26:32 +0300

    Added finding ebob

--- overview_4.js
+++ overview_4.js
@@ -25,7 +25,7 @@ while (estimated != randomNumber) {
 */
 
 // kullanıcı 0'a bastığında girilen sayıları çarpamını veren uygulama 
-
+/*
 let enterNumber = parseInt(prompt("You are enter a number"));
 let result = 1;
 
@@ -34,3 +34,26 @@ while ( enterNumber != 0) {
     enterNumber = parseInt(prompt("Enter a number"));
 }
 console.log("Multiplication result :" + result);
+*/
+
+// girilen iki sayının ebob' unu bulan uygulama
+
+let firstNumber = parseInt(prompt("You are enter a number"));
+let secondNumber = parseInt(prompt("You are enter a number"));
+
+let ebob = 1, control=2;
+
+while (control <= firstNumber && control <= secondNumber) {
+    if (firstNumber % control == 0 && secondNumber % control == 0) {
+        ebob = control;
+    }
+    control++;
+}
+
+if (ebob == 1) {
+    console.log("These numbers are prime between them")
+} else {
+    console.log("Common divisor of these numbers : " + ebob)
+}
+
+
