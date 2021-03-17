0eb35957ceb4fd6fc53ac19030cfc1a55e5742aa
Joseph Abraham <ekurubal24@gmail.com>
2021-03-17 18:38:20 +0300

    Added primire number

---
+++ overview_5.js
@@ -0,0 +1,17 @@
+// Girilen integer sayıya kadar olan asal sayıları bulan uygulama
+
+let enterNumber = parseInt(prompt("You are enter a number", "10"));
+
+for (i = 2; i <= enterNumber; i++) {
+
+    let primireNumber = true;
+
+    for (j = 2; j < i; j++) {
+        if (i % j ==0) {
+            primireNumber = false;
+            break;
+        }
+    } if ( primireNumber == true) {
+        console.log(i);
+    }
+}
